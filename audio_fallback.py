072dacd1b97fd3eede5e5f5f85d61c4951d087f5
nicole <nicolepas@al.insper.edu.br>
2026-01-27 18:34:48 -0300

    commit-nicole

--- audio_fallback.py
+++ audio_fallback.py
@@ -19,3 +19,12 @@ class SilentChannel:
     def set_volume(self, v): pass
     def get_busy(self): return False
 
+class SilentSound:
+    def _init_(self, name="<silent>"):
+        self._name = name
+    def play(self, loops=0):
+        return SilentChannel()
+    def stop(self): pass
+    def set_volume(self, v): pass
+    def get_length(self): return 0.0
+    def _repr_(self): return f"<SilentSound {self._name}>"
